7e36f7b6163a16348272f17e483727559c9aa712
amaozhao <amaozhao@gmail.com>
2017-02-11 01:10:12 +0800

    添加注释

--- Module 1/Chapter 1/preprocessing.py
+++ Module 1/Chapter 1/preprocessing.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 import numpy as np
 from sklearn import preprocessing
 
@@ -9,26 +11,31 @@ data = np.array(
     ]
 )
 
-# mean removal
+# mean removal(去除均值)
 data_standardized = preprocessing.scale(data)
-print ("\nMean =", data_standardized.mean(axis=0))
-print ("Std deviation =", data_standardized.std(axis=0))
+print ("\nStandardize(标准化) =", data_standardized)
+print ("\nMean(均值) =", data_standardized.mean(axis=0))
+print ("Std deviation(标准方差) =", data_standardized.std(axis=0))
 
-# min max scaling
+# min max scaling(极值标准化法)
+# 参数 feature_range 表示最小, 最大值
 data_scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
 data_scaled = data_scaler.fit_transform(data)
-print ("\nMin max scaled data:\n", data_scaled)
+print ("\nMin max scaled data(极值标准化):\n", data_scaled)
 
-# normalization
-data_normalized = preprocessing.normalize(data, norm='l1')
-print ("\nL1 normalized data:\n", data_normalized)
+# normalization(正则化)
+data_normalized1 = preprocessing.normalize(data, norm='l1')
+data_normalized2 = preprocessing.normalize(data, norm='l2')
+print ("\nL1 normalized data1:\n", data_normalized1)
+print ("\nL2 normalized data2:\n", data_normalized2)
 
-# binarization
+# binarization(二值化)
+# threshold 的意思是: 阀值(小于时设置为0, 大于时设置为1)
 data_binarized = preprocessing.Binarizer(threshold=1.4).transform(data)
-print ("\nBinarized data:\n", data_binarized)
+print ("\nBinarized data(二值化):\n", data_binarized)
 
-# one hot encoding
+# one hot encoding(独热编码或一位有效编码)
 encoder = preprocessing.OneHotEncoder()
 encoder.fit([[0, 2, 1, 12], [1, 3, 5, 3], [2, 3, 2, 12], [1, 2, 4, 3]])
 encoded_vector = encoder.transform([[2, 3, 5, 3]]).toarray()
-print ("\nEncoded vector:\n", encoded_vector)
+print ("\nEncoded vector(独热编码):\n", encoded_vector)

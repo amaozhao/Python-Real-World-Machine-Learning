9e5a6fd2b7a95844a031a15fbc67e65010c1fff1
amaozhao <amaozhao@gmail.com>
2017-02-11 02:01:33 +0800

    update

--- Module 1/Chapter 1/label_encoder.py
+++ Module 1/Chapter 1/label_encoder.py
@@ -1,23 +1,26 @@
-import numpy as np
+# coding: utf-8
+
+# import numpy as np
 from sklearn import preprocessing
 
+# 标签编码
 label_encoder = preprocessing.LabelEncoder()
 input_classes = ['audi', 'ford', 'audi', 'toyota', 'ford', 'bmw']
 label_encoder.fit(input_classes)
 
 # print classes
-print "\nClass mapping:"
+print ("\nClass mapping:")
 for i, item in enumerate(label_encoder.classes_):
-    print item, '-->', i
+    print (item, '-->', i)
 
-# transform a set of classes
+# transform a set of classes(分类转换)
 labels = ['toyota', 'ford', 'audi']
 encoded_labels = label_encoder.transform(labels)
-print "\nLabels =", labels 
-print "Encoded labels =", list(encoded_labels)
+print ("\nLabels =", labels)
+print ("Encoded labels =", list(encoded_labels))
 
-# inverse transform
+# inverse transform(反转)
 encoded_labels = [2, 1, 0, 3, 1]
 decoded_labels = label_encoder.inverse_transform(encoded_labels)
-print "\nEncoded labels =", encoded_labels
-print "Decoded labels =", list(decoded_labels)
+print ("\nEncoded labels =", encoded_labels)
+print ("Decoded labels =", list(decoded_labels))
--- Module 1/Chapter 1/regression_multivar.py
+++ Module 1/Chapter 1/regression_multivar.py
@@ -1,6 +1,10 @@
-import sys
+# coding: utf-8
 
+import sys
 import numpy as np
+from sklearn import linear_model
+import sklearn.metrics as sm
+from sklearn.preprocessing import PolynomialFeatures
 
 filename = sys.argv[1]
 X = []
@@ -17,20 +21,22 @@ num_training = int(0.8 * len(X))
 num_test = len(X) - num_training
 
 # Training data
-#X_train = np.array(X[:num_training]).reshape((num_training,1))
+# X_train = np.array(X[:num_training]).reshape((num_training, -1))
 X_train = np.array(X[:num_training])
-y_train = np.array(y[:num_training])
+y_train = np.array(y[:num_training]).reshape((num_training, -1))
 
 # Test data
-#X_test = np.array(X[num_training:]).reshape((num_test,1))
+# X_test = np.array(X[num_training:]).reshape((num_test, -1))
 X_test = np.array(X[num_training:])
-y_test = np.array(y[num_training:])
+y_test = np.array(y[num_training:]).reshape((num_test, -1))
 
-# Create linear regression object
-from sklearn import linear_model
 
 linear_regressor = linear_model.LinearRegression()
-ridge_regressor = linear_model.Ridge(alpha=0.01, fit_intercept=True, max_iter=10000)
+ridge_regressor = linear_model.Ridge(
+    alpha=0.01,
+    fit_intercept=True,
+    max_iter=10000
+)
 
 # Train the model using the training sets
 linear_regressor.fit(X_train, y_train)
@@ -40,37 +46,57 @@ ridge_regressor.fit(X_train, y_train)
 y_test_pred = linear_regressor.predict(X_test)
 y_test_pred_ridge = ridge_regressor.predict(X_test)
 
-# Measure performance
-import sklearn.metrics as sm
 
-print "LINEAR:"
-print "Mean absolute error =", round(sm.mean_absolute_error(y_test, y_test_pred), 2) 
-print "Mean squared error =", round(sm.mean_squared_error(y_test, y_test_pred), 2) 
-print "Median absolute error =", round(sm.median_absolute_error(y_test, y_test_pred), 2) 
-print "Explained variance score =", round(sm.explained_variance_score(y_test, y_test_pred), 2) 
-print "R2 score =", round(sm.r2_score(y_test, y_test_pred), 2)
+print ("LINEAR:")
+print (
+    "Mean absolute error =",
+    round(sm.mean_absolute_error(y_test, y_test_pred), 2)
+)
+print (
+    "Mean squared error =",
+    round(sm.mean_squared_error(y_test, y_test_pred), 2)
+)
+print (
+    "Median absolute error =",
+    round(sm.median_absolute_error(y_test, y_test_pred), 2)
+)
+print (
+    "Explained variance score =",
+    round(sm.explained_variance_score(y_test, y_test_pred), 2)
+)
+print ("R2 score =", round(sm.r2_score(y_test, y_test_pred), 2))
 
-print "\nRIDGE:"
-print "Mean absolute error =", round(sm.mean_absolute_error(y_test, y_test_pred_ridge), 2) 
-print "Mean squared error =", round(sm.mean_squared_error(y_test, y_test_pred_ridge), 2) 
-print "Median absolute error =", round(sm.median_absolute_error(y_test, y_test_pred_ridge), 2) 
-print "Explained variance score =", round(sm.explained_variance_score(y_test, y_test_pred_ridge), 2) 
-print "R2 score =", round(sm.r2_score(y_test, y_test_pred_ridge), 2)
+print ("\nRIDGE:")
+print (
+    "Mean absolute error =",
+    round(sm.mean_absolute_error(y_test, y_test_pred_ridge), 2)
+)
+print (
+    "Mean squared error =",
+    round(sm.mean_squared_error(y_test, y_test_pred_ridge), 2)
+)
+print (
+    "Median absolute error =",
+    round(sm.median_absolute_error(y_test, y_test_pred_ridge), 2)
+)
+print (
+    "Explained variance score =",
+    round(sm.explained_variance_score(y_test, y_test_pred_ridge), 2)
+)
+print ("R2 score =", round(sm.r2_score(y_test, y_test_pred_ridge), 2))
 
-# Polynomial regression
-from sklearn.preprocessing import PolynomialFeatures
 
 polynomial = PolynomialFeatures(degree=10)
 X_train_transformed = polynomial.fit_transform(X_train)
-datapoint = [0.39,2.78,7.11]
+datapoint = np.array([0.39, 2.78, 7.11]).reshape((1, -1))
 poly_datapoint = polynomial.fit_transform(datapoint)
 
 poly_linear_model = linear_model.LinearRegression()
 poly_linear_model.fit(X_train_transformed, y_train)
-print "\nLinear regression:\n", linear_regressor.predict(datapoint)
-print "\nPolynomial regression:\n", poly_linear_model.predict(poly_datapoint)
+print ("\nLinear regression:\n", linear_regressor.predict(datapoint))
+print ("\nPolynomial regression:\n", poly_linear_model.predict(poly_datapoint))
 
 # Stochastic Gradient Descent regressor
 sgd_regressor = linear_model.SGDRegressor(loss='huber', n_iter=50)
-sgd_regressor.fit(X_train, y_train)
-print "\nSGD regressor:\n", sgd_regressor.predict(datapoint)
+sgd_regressor.fit(X_train, y_train.reshape((-1,)))
+print ("\nSGD regressor:\n", sgd_regressor.predict(datapoint))

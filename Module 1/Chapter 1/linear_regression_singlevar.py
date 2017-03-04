47392ac1c21df5b2fac5da88cc488e4510b0d793
amaozhao <amaozhao@gmail.com>
2017-03-04 10:25:34 +0800

    update

--- Module 1/Chapter 1/linear_regression_singlevar.py
+++ Module 1/Chapter 1/linear_regression_singlevar.py
@@ -1,6 +1,10 @@
 import sys
 
 import numpy as np
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+import sklearn.metrics as sm
+import cPickle as pickle
 
 filename = sys.argv[1]
 X = []
@@ -16,16 +20,14 @@ num_training = int(0.8 * len(X))
 num_test = len(X) - num_training
 
 # Training data
-X_train = np.array(X[:num_training]).reshape((num_training,1))
+X_train = np.array(X[:num_training]).reshape((num_training, 1))
 y_train = np.array(y[:num_training])
 
 # Test data
-X_test = np.array(X[num_training:]).reshape((num_test,1))
+X_test = np.array(X[num_training:]).reshape((num_test, 1))
 y_test = np.array(y[num_training:])
 
 # Create linear regression object
-from sklearn import linear_model
-
 linear_regressor = linear_model.LinearRegression()
 
 # Train the model using the training sets
@@ -35,8 +37,6 @@ linear_regressor.fit(X_train, y_train)
 y_test_pred = linear_regressor.predict(X_test)
 
 # Plot outputs
-import matplotlib.pyplot as plt
-
 plt.scatter(X_test, y_test, color='green')
 plt.plot(X_test, y_test_pred, color='black', linewidth=4)
 plt.xticks(())
@@ -44,17 +44,28 @@ plt.yticks(())
 plt.show()
 
 # Measure performance
-import sklearn.metrics as sm
-
-print "Mean absolute error =", round(sm.mean_absolute_error(y_test, y_test_pred), 2) 
-print "Mean squared error =", round(sm.mean_squared_error(y_test, y_test_pred), 2) 
-print "Median absolute error =", round(sm.median_absolute_error(y_test, y_test_pred), 2) 
-print "Explain variance score =", round(sm.explained_variance_score(y_test, y_test_pred), 2) 
-print "R2 score =", round(sm.r2_score(y_test, y_test_pred), 2)
+print (
+    "Mean absolute error =",
+    round(sm.mean_absolute_error(y_test, y_test_pred), 2)
+)
+print (
+    "Mean squared error =",
+    round(sm.mean_squared_error(y_test, y_test_pred), 2)
+)
+print (
+    "Median absolute error =",
+    round(sm.median_absolute_error(y_test, y_test_pred), 2)
+)
+print (
+    "Explain variance score =",
+    round(sm.explained_variance_score(y_test, y_test_pred), 2)
+)
+print (
+    "R2 score =",
+    round(sm.r2_score(y_test, y_test_pred), 2)
+)
 
 # Model persistence
-import cPickle as pickle
-
 output_model_file = '3_model_linear_regr.pkl'
 
 with open(output_model_file, 'w') as f:
@@ -64,5 +75,7 @@ with open(output_model_file, 'r') as f:
     model_linregr = pickle.load(f)
 
 y_test_pred_new = model_linregr.predict(X_test)
-print "\nNew mean absolute error =", round(sm.mean_absolute_error(y_test, y_test_pred_new), 2) 
-
+print (
+    "\nNew mean absolute error =",
+    round(sm.mean_absolute_error(y_test, y_test_pred_new), 2)
+)
